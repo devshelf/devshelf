df95299d1ef3d861ca82720d3a9eff27b5ab32de
Robert Haritonov <r@rhr.me>
2014-05-17 14:38:03 +0300

    Auto-suggest test

--- tests/search.js
+++ tests/search.js
@@ -1,3 +1,5 @@
+casper.options.viewportSize = {width: 1024, height: 768};
+
 casper.test.begin('Checking search', 3, function suite(test) {
 	casper.start('http://127.0.0.1:8888').then(function() {
 
@@ -15,6 +17,27 @@ casper.test.begin('Checking search', 3, function suite(test) {
 
         test.assertEquals(this.getCurrentUrl(), 'http://127.0.0.1:8888/#!/search/css', 'URL is right')
 
+
+	}).run(function() { test.done() }).clear();
+});
+
+casper.test.begin('Checking auto-suggest', 2, function suite(test) {
+	casper.start('http://127.0.0.1:8888').then(function() {
+
+		test.assertExists('.js-search-input', 'Search input exists');
+
+        this.fillSelectors('form.home-search', {
+            '.js-search-input':'css'
+        }, false);
+
+        this.sendKeys('.js-search-input', casper.page.event.key.Down);
+        this.sendKeys('.js-search-input', casper.page.event.key.Enter);
+
+    }).then(function() {
+
+        test.assertExists('#posts-output', 'Results exist after submit');
+
+//        this.capture('test.png');
 
 	}).run(function() { test.done() }).clear();
 });
