a54e0d1ef3e3e683ad0fc076a53a62abb04d0fdc
Robert Haritonov <r@rhr.me>
2014-05-31 21:09:26 +0300

    Localization tests

---
+++ tests/l18n.js
@@ -0,0 +1,21 @@
+casper.options.viewportSize = {width: 1024, height: 768};
+
+casper.test.begin('Checking localization', 3, function suite(test) {
+	casper.start('http://127.0.0.1:8888').then(function() {
+
+        test.assertTextExists('Thanks to', 'English localization is ON');
+
+        test.assertExists('.js-language.__ru', 'Language switching tumbler exists');
+
+        this.click('.js-language.__ru');
+
+    }).then(function() {
+
+//        this.capture('test.png');
+
+        casper.waitForSelector(".footer", function() {
+            test.assertTextExists('Спасибо команде', 'Russion localization is turning ON');
+        });
+
+	}).run(function() { test.done() }).clear();
+});
